3ef63f9776df096ca3d9b7817d3382e0de55f236
Fernando Washington <ironitabarbosa@gmail.com>
2025-04-30 16:12:03 -0400

    feat: add Person superclass with name, cpf and birth date

---
+++ person.py
@@ -0,0 +1,26 @@
+import datetime
+from typing import List
+from address import Address 
+
+class Person:
+    def __init__(self, name: str, cpf: str, address: List[Address], birth_date: datetime.date):
+        self._name = name
+        self._cpf = cpf
+        self._address = address
+        self._birth_date = birth_date
+
+    @property
+    def name(self): 
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        self._name = value
+
+    @property
+    def cpf(self):
+        return self._cpf
+
+    @property
+    def birth_date(self):
+        return self._birth_date
