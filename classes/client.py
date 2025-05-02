4bd13b46251a5d2d7ec87bce42b3b74efe903a5e
Fernando Washington <ironitabarbosa@gmail.com>
2025-05-02 17:58:59 -0400

    feat: add relationships between user and client and delivery_person

---
+++ classes/client.py
@@ -0,0 +1,60 @@
+from typing import List
+
+from user import User
+from item import Item
+from order import Order
+
+from person import Person
+
+
+class Client(Person):
+    def __init__(self, name, cpf, address, birth_date, order_history: List[Order], cart: List[Item], user: User):
+        super().__init__(name, cpf, address, birth_date)
+
+        self._order_history = order_history
+        self._cart = cart 
+        self._user = user
+
+    @property
+    def order_history(self):
+        return self._order_history
+
+    @property
+    def cart(self):
+        return self._cart
+
+    @cart.setter
+    def cart(self, value):
+        self._cart = value
+        
+    @property
+    def user(self):
+        return self._user
+
+    @user.setter
+    def user(self, value: User):
+        self._user = value
+
+    def finalize_order(self, item: Item):
+        if not self._cart:
+            print("Carrinho vazio! Não é possível finalizar o pedido.")
+            # remove print
+            return
+
+            # verificar a lógica com o pessoal pois vai precisar do resto do sistema antes de continuar os métodos
+
+    def add_item(self, item: Item):
+        self._cart.append(item)
+
+    def remove_item(self, item: Item):
+        if item in self._cart:
+            self._cart.remove(item)
+        else:
+            print("Carrinho vazio! Não é possível remover o pedido.")
+
+    def cancel_order(self):
+        self._cart.clear()
+
+
+class ManageCustomer:
+    pass  # crud
---
+++ classes/delivery_person.py
@@ -0,0 +1,68 @@
+from typing import List
+
+from user import User
+from order import Order
+from vehicle import Vehicle
+
+from person import Person
+
+
+class DeliveryPerson(Person):
+    def __init__(self, name, cpf, address, birth_date, cnh: None, available: bool, vehicle: List[Vehicle], accepted_orders: List[Order], user: User):
+        super().__init__(name, cpf, address, birth_date)
+        self._cnh = cnh
+        self._available = True
+        self._vehicle = vehicle
+        self._accepted_orders = []
+        self.user = user
+
+        @property
+        def cnh(self):
+            return self._cnh
+
+        @property
+        def available(self):
+            return self._available
+
+        @available.setter
+        def available(self, value):
+            if not isinstance(value, bool):
+                raise ValueError("O valor de 'available' deve ser um boleano.")
+            self._available = value
+
+        @property
+        def vehicle(self):
+            return self._vehicle
+
+        @vehicle.setter
+        def vehicle(self, value):
+            self._vehicle = value
+
+        @property
+        def accepted_orders(self):
+            return self._accepted_orders
+
+        @accepted_orders.setter
+        def accepted_orders(self, value):
+            self._accepted_orders = value
+            
+        @property
+        def user(self):
+            return self._user
+
+        @user.setter
+        def user(self, value: User):
+            self._user = value
+
+
+        def accept_order(self, order: Order):
+            if self.available:
+                self._accepted_orders.append(order)
+                self.available = False
+                return f"Pedido {order} aceito."
+            else:
+                return f"{self.name} precisa entregar o pedido aceito."
+
+
+class DeliveryCustomer:
+    pass  # crud
