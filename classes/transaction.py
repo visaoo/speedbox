18059a398bb3e7455b03b727c21285e2472440c8
Toltz <joaovictorcarrenho@gmail.com>
2025-05-04 13:26:41 -0300

    adding class NFE and new features in transaction

--- classes/transaction.py
+++ classes/transaction.py
@@ -1,13 +1,15 @@
 from abc import ABC, abstractmethod
 from enum import Enum
 from uuid import uuid4
+from datetime import datetime
+
 
 class Transaction(ABC):
     def __init__(self, value, date, time_for_pay, payment_status):
-        self._value
-        self._date
-        self._time_for_pay
-        self._payment_status
+        self._value = value
+        self._date = date
+        self._time_for_pay = time_for_pay
+        self._payment_status = payment_status
         
 
     @property
@@ -85,7 +87,13 @@ class Card(Transaction):
 
     @number.setter
     def number(self, value):
-        self._number = value
+        cleaned_value = str(value).replace(' ', '').replace('-', '')
+        
+        if not cleaned_value.isdigit():
+            raise ValueError("O número deve conter apenas dígitos")
+        if len(cleaned_value) < 13 or len(cleaned_value) > 19:
+            raise ValueError("O número deve ter entre 13 e 19 dígitos")
+        self._number = cleaned_value
 
     @property
     def validity(self):
@@ -101,15 +109,86 @@ class Card(Transaction):
 
     @cvc.setter
     def cvc(self, value):
+        if len(value) != 3:
+            raise ValueError('CVC diferente de 3')
         self._cvc = value
-
+    
     @property
     def flag(self):
         return self._flag
+    
+    
+    
+    def identificar_bandeira(self):
+        
+        # Identificação por prefixo e comprimento
+        if (self.number.startswith('4') and 
+            len(self.number) in [13, 16]):
+            self.flag = "Visa"
+            return
+        elif ((self.number.startswith('51') or 
+            self.number.startswith('52') or 
+            self.number.startswith('53') or 
+            self.number.startswith('54') or 
+            self.number.startswith('55')) and 
+            len(self.number) == 16):
+            self.flag = "Mastercard"
+            return 
+        elif ((self.number.startswith('34') or 
+            self.number.startswith('37')) and 
+            len(self.number) == 15):
+            self.flag = "American Express"
+            return 
+        elif ((self.number.startswith('300') or 
+            self.number.startswith('301') or 
+            self.number.startswith('302') or 
+            self.number.startswith('303') or 
+            self.number.startswith('304') or 
+            self.number.startswith('305') or 
+            self.number.startswith('36') or 
+            self.number.startswith('38') or 
+            self.number.startswith('39')) and 
+            len(self.number) in [14, 15, 16]):
+            self.flag = "Diners Club"
+            return 
+        elif (self.number.startswith('6011') and 
+            len(self.number) == 16):
+            self.flag = "Discover"
+            return 
+        elif ((self.number.startswith('35') and 
+            len(self.number) in [16, 17, 18, 19])):
+            self.flag = "JCB"
+            return 
+        elif ((self.number.startswith('22') and 
+            len(self.number) in [16])):
+            self.flag = "Mastercard (novos binários)"
+            return 
+        else:
+            return "Bandeira não identificada"
 
-    @flag.setter
-    def flag(self, value):
-        self._flag = value
+        
+    
+    def _validade_card_number(self):
+        digits = [int(d) for d in str(self._number)][::-1]
+        total = 0
+        for i, digit in enumerate(digits):
+            if i % 2 == 1:
+                doubled = digit * 2
+                total += doubled - 9 if doubled > 9 else doubled
+            else:
+                total += digit
+        return total % 10 == 0
+
+    def _validate_validity(self):
+        try:
+            exp_month, exp_year = self._validity.split("/")
+            exp_month = int(exp_month)
+            exp_year = int("20" + exp_year) if len(exp_year) == 2 else int(exp_year)
+            now = datetime.now()
+            return exp_year > now.year or (exp_year == now.year and exp_month >= now.month)
+        except:
+            return False
+
 
 
 class Boleto(Transaction):
@@ -135,7 +214,7 @@ class Boleto(Transaction):
     def typeable_line(self, value):
         self._typeable_line = value
 
-class Type_Key_Pix(Enum):
+class TypeKeyPix(Enum):
     EMAIL = 'email'
     UUID = uuid4()
     CELPHONE = 'celphone'
@@ -165,4 +244,5 @@ class Pix(Transaction):
     @key_type.setter
     def key_type(self, value):
         self._key_type = value
+
 
