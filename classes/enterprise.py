a7701c4faae0aa86cd1b999282e6d6234a4d9792
Toltz <joaovictorcarrenho@gmail.com>
2025-05-04 11:24:38 -0300

    commit adding classes NFE, enterprise, transation and add db

---
+++ classes/enterprise.py
@@ -0,0 +1,35 @@
+from Address import Address
+
+
+class Enterprise:
+    def __init__(self, name:str, cnpj:str, address:Address):
+        self._name = name
+        self._cnpj = cnpj
+        self._address = address
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        self._name = value
+
+    @property
+    def cnpj(self):
+        return self._cnpj
+
+    @cnpj.setter
+    def cnpj(self, value):
+        self._cnpj = value
+
+    @property
+    def address(self):
+        return self._address
+
+    @address.setter
+    def address(self, value):
+        self._address = value
+
+        
+        
